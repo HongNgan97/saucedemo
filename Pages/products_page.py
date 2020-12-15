49a7b2cb73b79a469e98e320f37b27e4d3cf5f65
hongngan18597 <hongngan18597@gmail.com>
2020-12-15 23:51:47 +0700

    Ngan commit code 15/12

--- Pages/products_page.py
+++ Pages/products_page.py
@@ -14,3 +14,21 @@ class ProductsPage(BasePage):
 
     def count_broken_images(self):
         return self.get_elements_size(ProductsPageLocators.IMAGE_BROKEN)
+
+    def get_badge_total(self):
+        index = 0
+        try:
+            text = self.get_text(ProductsPageLocators.ICON_BADGE_HAS_ITEMS)
+            index = int(text)
+        except:
+            return 0
+        return index
+
+    def click_badge_icon(self):
+        self.click(ProductsPageLocators.ICON_BADGE_NO_ITEM)
+
+    def click_remove_button(self, index):
+        self.click(ProductsPageLocators.BUTTON_REMOVE(index))
+
+    def click_add_to_cart_button(self, index):
+        self.click(ProductsPageLocators.BUTTON_ADD_TO_CART(index))
---
+++ Testcases/test_products_01.py
@@ -0,0 +1,38 @@
+import sys
+
+sys.path.append(".")
+
+import unittest
+# import time
+
+from Pages.login_page import LoginPage
+from Pages.products_page import ProductsPage
+from Testcases.base_test import BaseTest
+from Testdata.data import Data
+from Objects.account import Account
+
+
+class TestProduct(BaseTest):
+    @classmethod
+    def setUp(self):
+        super().setUp()
+
+    @classmethod
+    def tearDown(self):
+        super().tearDown()
+
+    def test_product(self):
+        login_page = LoginPage(self.driver)
+        account = Account('standard_user', Data.PASSWORD)
+        login_page.login(account)
+
+        products_page = ProductsPage(self.driver)
+        # total = products_page.get_badge_total()
+        # print(total)
+        # products_page.click_badge_icon()
+        products_page.click_add_to_cart_button(1)
+        products_page.click_remove_button(1)
+
+
+if __name__ == "__main__":
+    unittest.main()
